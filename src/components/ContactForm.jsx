d341d9c2fa6dfc0ddea2246304b2973846369423
PhaseOfficial <97903370+PhaseOfficial@users.noreply.github.com>
2025-01-28 00:09:30 +0200

    update colour

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -66,7 +66,7 @@ const ContactForm = () => {
           id="message"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
           required
         />
       </div>
